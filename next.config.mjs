d3032ffe6b55b9d6333f1818628d45960110b866
Boris Zubchenko <boriszubc@gmail.com>
2023-09-16 20:08:37 +0300

    backup

--- next.config.mjs
+++ next.config.mjs
@@ -29,7 +29,22 @@ const securityHeaders = [
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
   headers: async () => [{ source: '/(.*)', headers: securityHeaders }],
+  images: {
+    domains: [
+      'public.blob.vercel-storage.com',
+      'res.cloudinary.com',
+      'abs.twimg.com',
+      'pbs.twimg.com',
+      'avatars.githubusercontent.com',
+      'www.google.com',
+      'flag.vercel.app',
+      'illustrations.popsy.co',
+    ],
+  },
 }
 
 export default nextConfig
