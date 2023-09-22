82356cc1012579894e2e4db986c773f82926024c
Boris Zubchenko <boriszubc@gmail.com>
2023-09-23 00:03:07 +0300

    revert

--- app/(marketing)/(home)/_ui/features.tsx
+++ app/(marketing)/(home)/_ui/features.tsx
@@ -1,4 +1,14 @@
+import {
+  BentoGrid,
+  BentoItem,
+  BentoItemContent,
+  BentoItemHeader,
+  BentoItemIcon,
+  BentoItemTitle,
+} from '@shared/ui/bento-grid'
+import { Spotlight, SpotlightItem } from '@shared/ui/spotlight'
 import { featureSections } from '@marketing/(home)/_lib/data'
+import { Badge } from '@shared/ui/badge'
 
 export function Features() {
   return (
@@ -17,37 +27,37 @@ export function Features() {
               {section.title}
             </h3>
           </header>
-          {/*<Spotlight*/}
-          {/*  size="large"*/}
-          {/*  color={section.spotlightColor}*/}
-          {/*>*/}
-          {/*  <BentoGrid*/}
-          {/*    className="container max-w-7xl"*/}
-          {/*    variant={section.bentoVariant}*/}
-          {/*  >*/}
-          {/*    {section.items.map((feature) => (*/}
-          {/*      <SpotlightItem key={feature.title}>*/}
-          {/*        <BentoItem className="relative">*/}
-          {/*          {feature.comingSoon && (*/}
-          {/*            <Badge*/}
-          {/*              className="absolute right-4 top-4"*/}
-          {/*              color={section.spotlightColor}*/}
-          {/*            >*/}
-          {/*              Coming Soon*/}
-          {/*            </Badge>*/}
-          {/*          )}*/}
-          {/*          <BentoItemHeader>*/}
-          {/*            <BentoItemIcon>{feature.icon}</BentoItemIcon>*/}
-          {/*            <BentoItemTitle>*/}
-          {/*              <h4>{feature.title}</h4>*/}
-          {/*            </BentoItemTitle>*/}
-          {/*          </BentoItemHeader>*/}
-          {/*          <BentoItemContent>{feature.content}</BentoItemContent>*/}
-          {/*        </BentoItem>*/}
-          {/*      </SpotlightItem>*/}
-          {/*    ))}*/}
-          {/*  </BentoGrid>*/}
-          {/*</Spotlight>*/}
+          <Spotlight
+            size="large"
+            color={section.spotlightColor}
+          >
+            <BentoGrid
+              className="container max-w-7xl"
+              variant={section.bentoVariant}
+            >
+              {section.items.map((feature) => (
+                <SpotlightItem key={feature.title}>
+                  <BentoItem className="relative">
+                    {feature.comingSoon && (
+                      <Badge
+                        className="absolute right-4 top-4"
+                        color={section.spotlightColor}
+                      >
+                        Coming Soon
+                      </Badge>
+                    )}
+                    <BentoItemHeader>
+                      <BentoItemIcon>{feature.icon}</BentoItemIcon>
+                      <BentoItemTitle>
+                        <h4>{feature.title}</h4>
+                      </BentoItemTitle>
+                    </BentoItemHeader>
+                    <BentoItemContent>{feature.content}</BentoItemContent>
+                  </BentoItem>
+                </SpotlightItem>
+              ))}
+            </BentoGrid>
+          </Spotlight>
         </article>
       ))}
     </div>
