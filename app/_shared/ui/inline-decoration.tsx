839c27c3d35470f10789a3ddc8b4951725cade64
Boris Zubchenko <boriszubc@gmail.com>
2023-09-23 20:56:51 +0300

    backup

--- app/_shared/ui/inline-decoration.tsx
+++ app/_shared/ui/inline-decoration.tsx
@@ -4,7 +4,9 @@ import { cn } from '@shared/lib/utils'
 export function InlineDecoration({ className, ...props }: SlotProps) {
   return (
     <Slot
-      className={cn('h-[1em] min-h-fit w-[1em] min-w-fit', { className })}
+      className={cn('max-h-[1em] min-h-fit min-w-fit max-w-[1em]', {
+        className,
+      })}
       {...props}
     />
   )
